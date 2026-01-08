704818044a712db6d75c9f05ec5a1d6625d1f7e1
Chandupa Marapana <136840067+chandupamarapana@users.noreply.github.com>
2026-01-08 08:46:23 +0530

    complete initial backend development and database connection with Docker

--- demo/src/main/java/com/example/demo/Repository/MovieRepository.java
+++ demo/src/main/java/com/example/demo/Repository/MovieRepository.java
@@ -2,10 +2,7 @@ package com.example.demo.Repository;
 
 import com.example.demo.Model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class MovieRepository extends JpaRepository<Movie, Long> {
-    // this is an empty repository gives us all the CRUD operations automatically
-
+public interface MovieRepository extends JpaRepository<Movie,Long> {
+    //this empty repo will give the crud operations automatically
 }
